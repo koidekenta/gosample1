d23ba7181585f9f69154db452cc139c0d395c101
koidekenta <30596400+koidekenta@users.noreply.github.com>
2020-12-16 07:00:36 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import(
   "gosample/mypkg"
-	"fmt"
+  "fmt"
 )
 
 
 func main(){
   p1 := mypkg.Person{Name: "Go太郎", Age: 10}
-	fmt.Println(p1.Name, p1.Age)
-	fmt.Println(mypkg.Add(3,5))
+  fmt.Println(p1.Name, p1.Age)
+  fmt.Println(mypkg.Add(3,5))
   fmt.Println(mypkg.TAX)
 }
